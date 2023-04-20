7a9392aea3771cee35f9ad7506b734d094d2ba82
moxime <mossonce@hotmail.com>
2023-04-20 13:12:57 +0200

    tilt

--- tilt.py
+++ tilt.py
@@ -1,5 +1,10 @@
 from importlib_metadata import requires
-import os, sys, argparse, util, model, torch
+import os
+import sys
+import argparse
+import util
+import model
+import torch
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -12,6 +17,8 @@ from sklearn.decomposition import PCA
 plt.style.use('seaborn-whitegrid')
 
 # plots recon for ood close to sphere (prior)
+
+
 def show_outlier(x, x_out, kld, r, threshold):
     for j in range(kld.shape[0]):
         if kld[j] <= threshold:
@@ -24,12 +31,14 @@ def show_outlier(x, x_out, kld, r, threshold):
             ax2.imshow(x_out[j].detach().cpu().numpy()[0], cmap='gray')
             plt.show()
 
-# show latent point 
+# show latent point
+
+
 def show_latent(z_train, z_test, z_ood, j):
     c = ['k', 'dodgerblue', 'mediumseagreen']
     m = ['o', '^', 'x']
     s = [9, 14, 14]
-    plt.figure(figsize=(4,4))
+    plt.figure(figsize=(4, 4))
 
     for i, z in enumerate([z_train, z_test, z_ood]):
         # plot points
@@ -45,7 +54,7 @@ def show_latent(z_train, z_test, z_ood, j):
         x1 = z @ v[0]
         x2 = z @ v[1]
         x = np.vstack((x1, x2)).T
-        
+
         # correct for distances
         zd = np.linalg.norm(z, axis=1)
         xd = np.linalg.norm(x, axis=1)
@@ -55,37 +64,40 @@ def show_latent(z_train, z_test, z_ood, j):
         x = np.vstack((x1, x2)).T
 
         plt.scatter(x1[:n], x2[:n], s=s[i], color=c[i], marker=m[i])
-    
+
     lim = 35
     plt.ylim([-lim, lim])
     plt.xlim([-lim, lim])
 
-    if j == 0: plt.legend(['Train', 'Test - ID', 'Test - OOD'])
+    if j == 0:
+        plt.legend(['Train', 'Test - ID', 'Test - OOD'])
     out = np.stack((z_train.cpu().detach().numpy(), z_test.cpu().detach().numpy(), z_ood.cpu().detach().numpy()))
-    #print(out.shape)
+    # print(out.shape)
     #np.save('latent/z_{}'.format(j), out)
     plt.tight_layout()
     # plt.savefig('latent/img {}.pdf'.format(j))
     plt.close()
 
 # main backprop loop
+
+
 def nest(all_loaders, nets, loss_fns, dist, device='cuda'):
     ood_loader, test_loader, train_loader = all_loaders
     netE, netD = nets
     in_loss_fn, out_loss_fn = loss_fns
 
     # backprop samples before test
-    for i, (x_ood, _) in enumerate(ood_loader):   
+    for i, (x_ood, _) in enumerate(ood_loader):
         # get in dist test batch (not seen during training)
         x_in, _ = next(iter(test_loader))
         x_in = x_in.to(device)
         x_ood = x_ood.to(device)
 
         # backprop
-        optimizer1 = optim.Adam(netE.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=3e-5) 
-        optimizer2 = optim.Adam(netD.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=3e-5) 
+        optimizer1 = optim.Adam(netE.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=3e-5)
+        optimizer2 = optim.Adam(netD.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=3e-5)
 
-        # main backprop loop 
+        # main backprop loop
         ne = 25 if n == 6 else 5
         for j in range(ne):
             # regular train pass
@@ -100,7 +112,7 @@ def nest(all_loaders, nets, loss_fns, dist, device='cuda'):
             x_out_train = netD(z_train)
             recon_train, kld_train = in_loss_fn(x_train, x_out_train, mu_train, logvar_train)
             loss = 1 * (recon_train + kld_train)
-        
+
             # ood pass
             opt_beta = 0.1
             z_ood, mu_ood, logvar_ood = netE(x_ood)
@@ -111,7 +123,7 @@ def nest(all_loaders, nets, loss_fns, dist, device='cuda'):
 
             # in dist pass
             z_in, mu_in, logvar_in = netE(x_in)
-            x_out_in = netD(z_in) # TODO: out in isn't good naming 
+            x_out_in = netD(z_in)  # TODO: out in isn't good naming
             recon_in, kld_in = out_loss_fn(x_in, x_out_in, mu_in, logvar_in)
             loss += opt_beta * (recon_in + kld_in)
 
@@ -121,11 +133,14 @@ def nest(all_loaders, nets, loss_fns, dist, device='cuda'):
             optimizer2.step()
 
             # visualize
-            if j == 0: show_latent(mu_train, mu_in, mu_ood, i)
-        if i >= 50: break 
+            if j == 0:
+                show_latent(mu_train, mu_in, mu_ood, i)
+        if i >= 50:
+            break
     return netE, netD
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_name', default=None, help='path to model checkpoint')
     parser.add_argument('--dataroot', default='../ood/data', help='path to dataset')
@@ -138,20 +153,21 @@ if __name__== "__main__":
     parser.add_argument('--workers', type=int, default=4, help='number of data loading workers')
     parser.add_argument('--aucroc', type=bool, default=True, help='boolean, run aucroc testing')
     parser.add_argument('--device')
-    
+
     opt = parser.parse_args()
 
     if opt.test_name == None:
         raise ValueError('enter a load folder')
- 
+
     cudnn.benchmark = True
     device = opt.device if torch.cuda.is_available() else 'cpu'
     print(device)
-    
+
     # information from test_name files
     load_path = os.path.join('results', opt.test_name)
-    [train_dataset, loss_type, tilt, nz]= util.get_test_info(load_path)
-    tilt = torch.tensor(float(tilt)) if tilt != None else None
+    [train_dataset, loss_type, tilt, nz] = util.get_test_info(load_path)
+    tilt = torch.tensor(float(tilt)) if tilt else None
+    print('Tilt is {}'.format(tilt))
     if tilt is None and opt.dist == 0 and opt.nest == 1:
         print('error: using tilt and dist = 0')
         sys.exit()
@@ -160,16 +176,16 @@ if __name__== "__main__":
     print('latent dims: {}, tilt: {}'.format(nz, tilt))
 
     # load datasets
-    loaders,loader_names,image_size = util.load_datasets(train_dataset, opt.dataroot,
-                                                         batch_size=opt.batch_size, num_workers=opt.workers)    
-    train_loader = loaders[0] 
-    test_loader = loaders[1] 
+    loaders, loader_names, image_size = util.load_datasets(train_dataset, opt.dataroot,
+                                                           batch_size=opt.batch_size, num_workers=opt.workers)
+    train_loader = loaders[0]
+    test_loader = loaders[1]
     print(*loader_names)
 
     # loss function
     print('loss:', loss_type)
-    in_loss_fn = model.Loss(loss_type, tilt, nz) 
-    out_loss_fn = model.Loss(loss_type, opt.ood_tilt, nz) 
+    in_loss_fn = model.Loss(loss_type, tilt, nz)
+    out_loss_fn = model.Loss(loss_type, opt.ood_tilt, nz)
 
     # make model, get state dict
     netE = model.Encoder(image_size, nz, tilt)
@@ -184,26 +200,28 @@ if __name__== "__main__":
         netE.load_state_dict(state_E)
         netD.load_state_dict(state_D)
         netE.to(device)
-        netD.to(device)       
+        netD.to(device)
         #        if n <= 3: continue
         print('testing dataset: {} of len {}'.format(loader_names[n], len(ood_loader) * opt.batch_size))
 
         # main backprop loop
-        # return upated network 
+        # return upated network
         if opt.nest == 1:
             print('nesting')
             all_loaders = (ood_loader, test_loader, train_loader)
             nets = (netE, netD)
             loss_fns = (in_loss_fn, out_loss_fn)
             netE, netD = nest(all_loaders, nets, loss_fns, opt.dist)
-        else: print('skipping nesting')
+        else:
+            print('skipping nesting')
 
         # main test loop
         all_track = []
         eval_loaders = (ood_loader, test_loader)
         for n, loader in enumerate(eval_loaders):
-            for i, (x, _) in enumerate(loader):   
-                if i > 20: break
+            for i, (x, _) in enumerate(loader):
+                if i > 20:
+                    break
                 x = x.to(device)
 
                 z, mu, logvar = netE(x)
@@ -212,8 +230,10 @@ if __name__== "__main__":
                 loss = (recon + kld).detach().cpu().numpy()
                 # print('loss {}: {:.2f} recon: {:2f} kl: {:.2f}'.format(['out', 'in'][n], loss.mean(), recon.mean(), kld.mean()))
                 # print('*** x:', *x.shape, 'x_out:', *x_out.shape, 'loss', loss)
-                if i == 0: track = loss
-                else: track = np.concatenate((track, loss))
+                if i == 0:
+                    track = loss
+                else:
+                    track = np.concatenate((track, loss))
             all_track.append(track)
 
         # NOTE: assumes same number of id and ood samples
@@ -221,36 +241,36 @@ if __name__== "__main__":
         labels = np.concatenate((np.ones_like(all_track[0]), np.zeros_like(all_track[1])))
         fpr, tpr, _ = metrics.roc_curve(labels, scores)
         for t, f in zip(tpr, fpr):
-            if t > 0.95: break
-    
+            if t > 0.95:
+                break
+
         auc = metrics.auc(fpr, tpr)
         print('auc: {:.1%} fpr@{:.1%}={:.1%}'.format(auc, t, f))
         print()
 
-        #if opt.aucroc:
+        # if opt.aucroc:
         #    util.aucroc(opt.test_name, 'tilt', loader_names, train_dataset)
 
-            #if i == 0:
-                #recon_track = recon.cpu().numpy()
-                #kld_track = kld.cpu().numpy()
-            #else:
-                #recon_track = np.concatenate((recon_track, recon.cpu().numpy()))
-                #kld_track = np.concatenate((kld_track, kld.cpu().numpy()))
+        # if i == 0:
+        #recon_track = recon.cpu().numpy()
+        #kld_track = kld.cpu().numpy()
+        # else:
+        #recon_track = np.concatenate((recon_track, recon.cpu().numpy()))
+        #kld_track = np.concatenate((kld_track, kld.cpu().numpy()))
 
-            #if i >= opt.samples or i == len(loader)-1: #test for _ batches
-                #save_path = os.path.join(load_path, 'aucroc', 'tilt')
-                #os.makedirs(save_path, exist_ok=True)
-                #np.save(os.path.join(save_path, loader_names[n] + ' recon_score.npy'), recon_track) 
-                #np.save(os.path.join(save_path, loader_names[n] + ' kld_score.npy'), kld_track)
-                #break        
-
+        # if i >= opt.samples or i == len(loader)-1: #test for _ batches
+        #save_path = os.path.join(load_path, 'aucroc', 'tilt')
+        #os.makedirs(save_path, exist_ok=True)
+        #np.save(os.path.join(save_path, loader_names[n] + ' recon_score.npy'), recon_track)
+        #np.save(os.path.join(save_path, loader_names[n] + ' kld_score.npy'), kld_track)
+        # break
 
 
 #########
 # old code
 #########
 
-## for showing outliers
+# for showing outliers
 #r, q = loss_fn(x_a, x_out_a, mu_a, logvar_a, ood=True)
 #print(torch.mean(r) + 3 * torch.std(r))
 #r_temp, kld_temp = loss_fn(x, x_out, mu, logvar, ood=True)
